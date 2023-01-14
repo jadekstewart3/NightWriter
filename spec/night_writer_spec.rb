0e4727043bc77dec15154ec75aaaa1fb5dde1d3d
Jade Stewart <jadekstewart3@gmail.com>
2023-01-14 11:38:12 -0700

    Write initialize test for NightWriter class

---
+++ spec/night_writer_spec.rb
@@ -0,0 +1,22 @@
+require_relative 'spec_helper'
+
+RSpec.describe NightWriter do
+ 
+ let(:night_writer) { NightWriter.new }
+
+ before do
+   night_writer.read_file = './message.txt'
+   night_writer.write_file = './braille.txt'
+ end
+ 
+ describe '#initalize' do
+   it 'exists' do
+     expect(night_writer).to be_a(NightWriter)
+   end
+ 
+   it 'has attributes' do
+     night_writer.read_and_write
+      expect(night_writer.read_file).to eq('./message.txt')
+     expect(night_writer.write_file).to eq('./braille.txt')
+   end
+ end
