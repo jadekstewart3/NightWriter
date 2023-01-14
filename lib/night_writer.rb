bf7171cfb15c71230417ad521d1ac7cf9fe59ef8
Jade Stewart <jadekstewart3@gmail.com>
2023-01-14 11:41:38 -0700

    Create NightWriter class and initalization

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,15 +1,22 @@
-message = File.open(ARGV[0], "r")
-
-incoming_message_text = message.read
-
-message.close
-
-text = incoming_message_text
-
-writer = File.open(ARGV[1], "w")
-
-puts "Created #{ARGV[1]} containing #{text.split('').count} characters."
-
-writer.write(text)
-
-writer.close
+class NightWriter
+   attr_accessor :read_file,
+                 :write_file
+ 
+ def initialize
+   @read_file = ARGV[0]
+   @write_file = ARGV[1]
+ end
+ 
+ def read_and_write
+   message = File.open(@read_file, "r")
+ 
+   text = message.read
+    puts "Created #{@write_file} containing #{text.split('').count} characters"
+   File.write(@write_file, text)
+ end
+end
+# night_writer = NightWriter.new
+# night_writer.read_file = './message.txt' if night_writer.read_file.nil?
+# night_writer.write_file = './braille.txt' if night_writer.write_file.nil?
+# night_writer.read_and_write
+#use this^^ to run from command line (runner file style)
