223bfe7e8a8274b4e1c58098d60c5e149b640ceb
Jade Stewart <jadekstewart3@gmail.com>
2023-01-15 17:33:08 -0700

    Create helper methods to improve converter functionality

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,11 +1,13 @@
 class NightWriter
   attr_reader  :english_to_braille
   attr_accessor :read_file,
-                :write_file
+                :write_file,
+                :text
 
  def initialize
    @read_file = ARGV[0]
    @write_file = ARGV[1]
+   @text = File.open(@read_file, "r")
    @english_to_braille = {
          "a" => ["0.", "..", ".."],
          "b" => ["0.", "0.", ".."],
@@ -38,26 +40,55 @@ class NightWriter
  end
  
   def read_and_write_text
-    message = File.open(@read_file, "r")
-    
-    text = message.read
-    puts "Created #{@write_file} containing #{text.split('').count} characters"
-    
-    File.write(@write_file, braille_converter)
+    text = @text.read
+
+    substrings = get_substrings(text)
+    converted_braille = braille_converter(substrings)
+
+    puts "Created #{@write_file} containing #{text.split("").count} characters"
+    File.write(@write_file, converted_braille)
   end
 
-  def braille_converter 
-    text_message = File.read(@read_file).chars
-    braille_arrays = text_message.map do |letter|
-      english_to_braille[letter]
+  def braille_converter(substrings) 
+    braille_chars = substrings.map do |str|
+      str.chars.map do |char|
+        @english_to_braille[char]
+      end
     end
-    braille_arrays.transpose.map do |array|
-      array.join
+    braille_chars.map do |array|
+      compact_array = array.compact
+      compact_array.transpose.map(&:join)
     end.join("\n")
   end
+
+  def get_substrings(text)
+    strings = [""]
+
+    words = text.split
+    words = break_up_long_words(words)
+    words.each do |str|
+      unless strings[-1].length + str.length > 40
+        strings[-1] << "#{str} "
+      else 
+        strings << "#{str} "
+      end
+    end
+    strings.map(&:rstrip)
+  end
+
+  def break_up_long_words(words)
+    words.map do |word|
+      unless word.length > 40
+        word
+      else 
+        word.scan(/.{1,40}/)
+      end
+    end.flatten
+  end
+
 end
 # night_writer = NightWriter.new
-# night_writer.read_file = './message.txt' if night_writer.read_file.nil?
-# night_writer.write_file = './braille.txt' if night_writer.write_file.nil?
+# night_writer.read_file = "./message.txt" if night_writer.read_file.nil?
+# night_writer.write_file = "./braille.txt" if night_writer.write_file.nil?
 # night_writer.read_and_write_text
 #use this^^ to run from command line (runner file style)
