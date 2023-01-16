3dea6bb5f55d0d9155be6d16ca52c30392351f32
Jade Stewart <jadekstewart3@gmail.com>
2023-01-16 12:43:07 -0700

    Write get_substrings method with passing test

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,22 +1,50 @@
-class NightReader
-  attr_reader  :braille_to_english
-  attr_accessor :read_file,
-                :write_file
+require_relative 'braille_dictionary'
 
-  def initalize 
-    @read_file = ARGV[0]
-    @write_file = ARGV[1]
+class NightReader
+  include BrailleDictionary
+  attr_accessor :read_file,
+                :write_file,
+                :braille_text
+
+  def initialize(input, output)
+    @read_file = input
+    @write_file = output
+    @braille_text = File.open(@read_file, "r")
   end
 
   def read_and_write_braille
-   message = File.open(@read_file, "r")
+   braille_message = File.open(@read_file, "r")
+   
+    braille_text = braille_message.read
+    substrings = get_substrings(braille_text)
 
-    text = message.read
-    puts "Created #{@write_file} containing #{text.split('').count} characters"
-    File.write(@write_file, text)
+    puts "Created #{@write_file} containing #{braille_text.split('').count} characters"
+    File.write(@write_file, braille_text)
  end
+
+ def get_substrings(braille_text)
+    braille_strings = [""]
+    braille_substrings = braille_text.split
+    braille_substrings.each do |b_str|
+      unless braille_strings[-1].length + b_str.length > 40 
+        braille_strings[-1] << "#{b_str}"
+      else
+        braille_strings << "#{b_str}"
+      end
+    end
+  end
+
+  
+
+  #make each line an array
+  #split by first two elements in array group by 3(rows)
+  #rev transpose?
+
+
 end
-# night_reader = NightReader.new
+# input = ARGV[0]
+# output = AGRV[1]
+# night_reader = NightReader.new(input, output)
 # night_reader.read_file = "./braille.txt" if night_reader.read_file.nil?
 # night_reader.write_file = "./original_message.txt" if night_reader.write_file.nil?
 # night_reader.read_and_write_braille
--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -2,12 +2,7 @@ require_relative 'spec_helper'
 
 RSpec.describe NightReader do 
 
-  let(:night_reader) {NightReader.new}
-
-  before do
-   night_reader.write_file = './original_message.txt'
-   night_reader.read_file = './braille.txt'
- end
+  let(:night_reader) { NightReader.new('./braille.txt', './original_message.txt') }
 
  describe '#initialize' do 
     it 'exists' do 
@@ -16,15 +11,32 @@ RSpec.describe NightReader do
 
     it 'has attributes' do 
       night_reader.read_and_write_braille
-
-      expect(night_reader.write_file).to eq('./original_message.txt')
       expect(night_reader.read_file).to eq('./braille.txt')
+      expect(night_reader.write_file).to eq('./original_message.txt')
     end
   end
+
+  describe '#english_converter' do
+      it 'returns an array of substrings' do
+        braille_text = "0. .. .."
+    
+        expect(night_reader.get_substrings(braille_text)).to eq(["0.", "..", ".."])
+      end
+
+      xit 'breaks up braille characters over 40 caharacters long' do
+        braille_array = ["0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.
+................................................................................
+................................................................................
+0.
+..
+.."]
   
-  describe '#english_converter' do 
-    it 'can convert a single english letter to braille' do
-      expect(night_reader.english_converter(["0.", "..", ".."])).to eq("a")
-    end
- end
+        expect(night_reader.break_up_long_words(braille_array)).to eq(["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"])
+      end
+
+
+      xit 'can convert a single english letter to braille' do
+        expect(night_reader.english_converter).to eq("a")
+      end
+   end
 end
