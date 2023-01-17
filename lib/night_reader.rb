5218657e3df1d4f24016b2df6260262dd48ce867
Jade Stewart <jadekstewart3@gmail.com>
2023-01-17 10:34:32 -0700

    Instantiate INVERTED_DICTIONARY constant in  NightReader

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -2,6 +2,7 @@ require_relative 'braille_dictionary'
 
 class NightReader
   include BrailleDictionary
+  attr_reader :braille_to_english_hash
   attr_accessor :read_file,
                 :write_file,
                 :braille_text
@@ -10,17 +11,21 @@ class NightReader
     @read_file = input
     @write_file = output
     @braille_text = File.open(@read_file, "r")
+    @braille_to_english_hash = INVERTED_DICTIONARY
   end
 
   def read_and_write_braille
     braille_text = @braille_text.read
 
-    puts "Created #{@write_file} containing #{braille_text.split('').count} characters"
-    File.write(@write_file, braille_text)
+    braille_lines = break_up_braille_lines(braille_text)
+    english_string = translate_braille_line(braille_lines.flatten)
+  
+    puts "Created #{@write_file} containing #{english_string.chars.count} characters"
+    File.write(@write_file, english_string)
   end
 
   def translate_braille_char(strings)
-    INVERTED_DICTIONARY[strings]
+    @braille_to_english_hash[strings]
   end
 
   def translate_braille_line(strings)
@@ -37,11 +42,4 @@ class NightReader
     strings = text.split("\n")
     strings.each_slice(3).to_a
   end
-
-
 end
-# input = ARGV[0]
-# output = ARGV[1]
-# night_reader = NightReader.new(input, output)
-# night_reader.read_and_write_braille
-##use this to run from command line ^^
