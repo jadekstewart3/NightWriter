ebe15b3671be0da1a4b2b57253db207e6b13b541
Jade Stewart <jadekstewart3@gmail.com>
2023-01-16 17:22:42 -0700

    Adds method translate_braille_char with passing test

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -13,39 +13,20 @@ class NightReader
   end
 
   def read_and_write_braille
-   braille_message = File.open(@read_file, "r")
-   
-    braille_text = braille_message.read
-    substrings = get_substrings(braille_text)
+    braille_text = @braille_text.read
 
     puts "Created #{@write_file} containing #{braille_text.split('').count} characters"
     File.write(@write_file, braille_text)
- end
-
- def get_substrings(braille_text)
-    braille_strings = [""]
-    braille_substrings = braille_text.split
-    braille_substrings.each do |b_str|
-      unless braille_strings[-1].length + b_str.length > 40 
-        braille_strings[-1] << "#{b_str}"
-      else
-        braille_strings << "#{b_str}"
-      end
-    end
   end
 
-  
-
-  #make each line an array
-  #split by first two elements in array group by 3(rows)
-  #rev transpose?
-
+  def translate_braille_char(braille)
+    sliced_braille = braille.split("\n")
+    INVERTED_DICTIONARY[sliced_braille]
+  end
 
 end
 # input = ARGV[0]
-# output = AGRV[1]
+# output = ARGV[1]
 # night_reader = NightReader.new(input, output)
-# night_reader.read_file = "./braille.txt" if night_reader.read_file.nil?
-# night_reader.write_file = "./original_message.txt" if night_reader.write_file.nil?
 # night_reader.read_and_write_braille
 ##use this to run from command line ^^
