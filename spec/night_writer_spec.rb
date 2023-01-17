4c13f3bb0832af577200ffee276f2eabd509f0d4
Jade Stewart <jadekstewart3@gmail.com>
2023-01-17 10:38:47 -0700

    Edit file paths for fixture files and add test for english_to_braille_hash

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -11,40 +11,45 @@ RSpec.describe NightWriter do
  
    it 'has attributes' do
      night_writer.read_and_write_text
+
      expect(night_writer.read_file).to eq('./message.txt')
      expect(night_writer.write_file).to eq('./braille.txt')
+     expect(night_writer.text).to be_a(File)
+     expect(night_writer.enlglish_to_braille_hash).to be_a(Hash)
    end
- end
+  end
  
-  describe '#braille_converter' do 
-    it 'can convert a single english letter to braille' do
-      expect(night_writer.braille_converter(["a"])).to eq("0.\n..\n..")
-    end
+ describe '#get_substrings' do 
+  it 'returns an array of substrings' do
+    file_contents = File.open('fixture/message_41_char_fixture.txt', 'r').read
+    
+    expect(night_writer.get_substrings(file_contents)).to eq(["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "a"])
+  end
+end
 
-    it 'can convert a multiple english letters to braille' do
-      expect(night_writer.braille_converter(["ab cde"])).to eq("0.0...00000.\n..0......0.0\n............")
-    end
+describe '#braille_converter' do 
+  it 'can convert a single english letter to braille' do
+    expect(night_writer.braille_converter(["a"])).to eq("0.\n..\n..")
   end
 
-  describe '#get_substrings' do 
-    it 'returns an array of substrings' do
-      file_contents = File.open('message_41_char_fixture.txt', 'r').read
-      expect(night_writer.get_substrings(file_contents)).to eq(["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "a"])
-    end
+  it 'can convert a multiple english letters to braille' do
+    
+    expect(night_writer.braille_converter(["ab cde"])).to eq("0.0...00000.\n..0......0.0\n............")
   end
+end
 
-  describe '#break_up_long_words' do 
-    it 'breaks english characters over 40 caharacters long' do
-      text_array = ["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"]
-  
-      expect(night_writer.break_up_long_words(text_array)).to eq(["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "a"])
+describe '#break_up_long_words' do 
+  it 'breaks english characters over 40 caharacters long' do
+    text_array = ["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"]
+    
+    expect(night_writer.break_up_long_words(text_array)).to eq(["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "a"])
     end
   end
 
   describe '#read_and_write_text' do 
     it 'returns translated english to braille text' do
       night_writer.read_and_write_text
-      expect(File.open('braille.txt', 'r').read).to match(File.open('braille_fixture.txt', 'r').read)
+      expect(File.open('braille.txt', 'r').read).to match(File.open('fixture/braille_fixture.txt', 'r').read)
     end
   end
 end
